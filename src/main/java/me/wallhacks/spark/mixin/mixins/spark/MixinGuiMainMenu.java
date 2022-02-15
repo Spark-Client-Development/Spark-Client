798b6064ac300f76562804f151b1fad60c078927
jandvd <68912659+jandvd@users.noreply.github.com>
2022-02-15 16:19:21 +0100

    fixed bugs

--- src/main/java/me/wallhacks/spark/mixin/mixins/spark/MixinGuiMainMenu.java
+++ src/main/java/me/wallhacks/spark/mixin/mixins/spark/MixinGuiMainMenu.java
@@ -1,6 +1,7 @@
 package me.wallhacks.spark.mixin.mixins.spark;
 
 import me.wallhacks.spark.Spark;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,4 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin({GuiMainMenu.class})
 public class MixinGuiMainMenu {
 
+    //we need this for some reason
+    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton, CallbackInfo info) {
+        if(Minecraft.getMinecraft().world != null)
+            return;
+        if (Spark.altManager.isMouseIn(mouseX)) info.cancel();
+    }
 }
--- src/main/java/me/wallhacks/spark/mixin/mixins/spark/MixinGuiMultiplayer.java
+++ src/main/java/me/wallhacks/spark/mixin/mixins/spark/MixinGuiMultiplayer.java
@@ -1,6 +1,7 @@
 package me.wallhacks.spark.mixin.mixins.spark;
 
 import me.wallhacks.spark.Spark;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiMultiplayer;
 import org.spongepowered.asm.mixin.Mixin;
