3bb1fa1a5a77f67b6a1a3325ee9f45579cbd6cbb
wallhacks0 <69912957+wallhacks0@users.noreply.github.com>
2022-02-18 16:30:01 +0100

    added FastSwim

--- src/main/java/me/wallhacks/spark/systems/module/modules/movement/FastSwim.java
+++ src/main/java/me/wallhacks/spark/systems/module/modules/movement/FastSwim.java
@@ -1,16 +1,21 @@
 package me.wallhacks.spark.systems.module.modules.movement;
 
+import me.wallhacks.spark.event.player.PacketSendEvent;
 import me.wallhacks.spark.event.player.PlayerMoveEvent;
 import me.wallhacks.spark.event.player.SneakEvent;
 import me.wallhacks.spark.systems.module.Module;
+import me.wallhacks.spark.systems.setting.settings.BooleanSetting;
 import me.wallhacks.spark.systems.setting.settings.DoubleSetting;
+import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module.Registration(name = "FastSwim", description = "Swim faster")
 public class FastSwim extends Module {
     public static FastSwim INSTANCE;
     public DoubleSetting waterSpeed = new DoubleSetting("WaterSpeed", this, 1.35, 1, 3);
-    public DoubleSetting lavaSpeed = new DoubleSetting("LavaSpeed", this, 2, 1, 3);
+    public DoubleSetting lavaSpeed = new DoubleSetting("LavaSpeed", this, 4.8, 1, 6);
+    BooleanSetting bypass = new BooleanSetting("Bypass", this, true);
+    boolean flag = false;
 
     public FastSwim() {
         INSTANCE = this;
@@ -18,18 +23,21 @@ public class FastSwim extends Module {
 
     @SubscribeEvent
     public void onMove(PlayerMoveEvent event) {
-        if ((mc.player.isInWater() || mc.player.isInWater()) && !mc.player.onGround) {
+        if ((mc.player.isInWater() || mc.player.isInLava()) && !mc.player.onGround) {
             boolean jump = mc.player.movementInput.jump;
             boolean sneak = mc.player.movementInput.sneak;
             mc.player.setSprinting(true);
             if (jump && !sneak) {
                 event.setY(0.11);
+                flag = false;
             } else if (!jump && sneak) {
                 mc.player.setSneaking(false);
-                event.setY(-0.11);
+                flag = false;
             } else {
-                if (mc.player.ticksExisted % 2 == 0) event.setY(-0.005);
-                else event.setY(0.005);
+                event.setY(0);
+                if (bypass.getValue())
+                    flag = !flag;
+                else flag = false;
             }
             if (mc.player.isInWater()) {
                 event.setX(event.getX() * waterSpeed.getFloatValue());
@@ -43,6 +51,14 @@ public class FastSwim extends Module {
 
     @SubscribeEvent
     public void onSneak(SneakEvent event) {
-        if ((mc.player.isInWater() || mc.player.isInWater()) && !mc.player.onGround) event.setCanceled(true);
+        if ((mc.player.isInWater() || mc.player.isInLava()) && !mc.player.onGround) event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public void onPacketSend(PacketSendEvent event) {
+        if (event.getPacket() instanceof CPacketPlayer.Position || event.getPacket() instanceof CPacketPlayer.PositionRotation) {
+            if ((mc.player.isInWater() || mc.player.isInLava()) && !mc.player.onGround)
+                if (flag) ((CPacketPlayer) event.getPacket()).y -= 0.005;
+        }
     }
 }
--- src/main/java/me/wallhacks/spark/systems/module/modules/movement/Sprint.java
+++ src/main/java/me/wallhacks/spark/systems/module/modules/movement/Sprint.java
@@ -1,20 +1,55 @@
 package me.wallhacks.spark.systems.module.modules.movement;
 
+import baritone.api.BaritoneAPI;
 import me.wallhacks.spark.event.player.PlayerLivingTickEvent;
+import me.wallhacks.spark.event.player.PlayerMoveEvent;
 import me.wallhacks.spark.systems.module.Module;
+import me.wallhacks.spark.systems.setting.settings.BooleanSetting;
+import me.wallhacks.spark.systems.setting.settings.ModeSetting;
 import me.wallhacks.spark.util.MC;
+import me.wallhacks.spark.util.player.PlayerUtil;
+import net.minecraft.init.MobEffects;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Arrays;
 
 @Module.Registration(name = "Sprint", description = "Sprint when we can")
 public class Sprint extends Module {
-
+    ModeSetting mode = new ModeSetting("Sprint", this, "Vanilla", Arrays.asList("Instant", "Vanilla"));
 
     @SubscribeEvent
-    public void onUpdate(PlayerLivingTickEvent event){
-
-        if(mc.player.movementInput.moveForward >= 0.8F)
+    public void onUpdate(PlayerLivingTickEvent event) {
+        if(mc.player.movementInput.moveForward >= 0.8F && mode.is("Vanilla"))
             mc.player.setSprinting(true);
+    }
 
+    @SubscribeEvent
+    public void onMove(PlayerMoveEvent event) {
+        if (mode.is("Instant") && !mc.player.isInLava() && !mc.player.isInWater()) {
+            float forward = mc.player.movementInput.moveForward;
+            float strafe = mc.player.movementInput.moveStrafe;
+            double speed = getBaseMotionSpeed();
+            if (forward == 0 && strafe == 0) {
+                event.setX(0D);
+                event.setZ(0D);
+            } else if (forward != 0.0D && strafe != 0.0D) {
+                forward *= Math.sin(0.7853981633974483D);
+                strafe *= Math.cos(0.7853981633974483D);
+            }
+            float yaw = BaritoneAPI.getProvider().getPrimaryBaritone().getLookBehavior().getYaw();
+            event.setX(forward * speed * -Math.sin(Math.toRadians(yaw)) + strafe * speed * Math.cos(Math.toRadians(yaw)));
+            event.setZ(forward * speed * Math.cos(Math.toRadians(yaw)) - strafe * speed * -Math.sin(Math.toRadians(yaw)));
+        }
+    }
+
+
+    private double getBaseMotionSpeed() {
+        double baseSpeed = 0.2873D;
+        if (mc.player.isPotionActive(MobEffects.SPEED)) {
+            int amplifier = mc.player.getActivePotionEffect(MobEffects.SPEED).getAmplifier();
+            baseSpeed *= 1.0D + 0.2D * ((double) amplifier + 1);
+        }
+        return baseSpeed;
     }
 
 }
