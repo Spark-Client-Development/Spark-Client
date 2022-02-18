092b83255129a247ba42aa72ff3799bce7ca34a5
TT392 <>
2022-02-18 17:27:33 +0100

    Add baritoneGoal hud item

---
+++ src/main/java/me/wallhacks/spark/systems/hud/huds/BaritoneGoal.java
@@ -0,0 +1,30 @@
+package me.wallhacks.spark.systems.hud.huds;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+
+import baritone.api.BaritoneAPI;
+import baritone.api.pathing.goals.GoalComposite;
+import me.wallhacks.spark.systems.hud.HudElement;
+import me.wallhacks.spark.systems.hud.InfoHudElement;
+import me.wallhacks.spark.systems.setting.settings.BooleanSetting;
+
+@HudElement.Registration(name = "BaritoneGoal", description = "Shows the current baritone goal", posX = 0.0, posY = 0.5, width = 30, height = 12)
+public class BaritoneGoal extends InfoHudElement {
+    BooleanSetting optifine = new BooleanSetting("Optifine", this, false);
+
+    @Override
+    public void draw(float deltaTime) {
+    	String Goal = "not active";
+        if (BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().isPathing()) {
+        	if (!(BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().getGoal() instanceof GoalComposite))
+        		Goal = BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().getGoal().toString();
+        	else
+        		Goal = "multiple goals";
+        		
+        }
+
+        setInfo(String.format(ChatFormatting.GRAY + "Baritone: %s%s", ChatFormatting.WHITE, Goal));
+
+        drawInfo();
+    }
+}
