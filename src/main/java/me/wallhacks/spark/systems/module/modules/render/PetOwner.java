bae8744fc94ffd7aeff30536ce37f713df44eccc
TT392 <>
2022-02-19 01:00:38 +0100

    Fancy rendering

--- src/main/java/me/wallhacks/spark/systems/module/modules/render/PetOwner.java
+++ src/main/java/me/wallhacks/spark/systems/module/modules/render/PetOwner.java
@@ -1,12 +1,14 @@
 package me.wallhacks.spark.systems.module.modules.render;
 
-import org.lwjgl.opengl.GL11;
+import java.awt.Color;
 
-import me.wallhacks.spark.Spark;
 import me.wallhacks.spark.systems.module.Module;
+import me.wallhacks.spark.systems.setting.settings.ColorSetting;
+import me.wallhacks.spark.systems.setting.settings.DoubleSetting;
 import me.wallhacks.spark.util.SessionUtils;
 import me.wallhacks.spark.util.render.RenderUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.AbstractHorse;
 import net.minecraft.entity.passive.EntityTameable;
@@ -14,36 +16,81 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Module.Registration(name = "PetOwner", description = "Renders logout spots for enemy", alwaysListening = true)
+@Module.Registration(name = "PetOwner", description = "Displays the owner of tamed entities")
 public class PetOwner extends Module {
+	public DoubleSetting scale = new DoubleSetting("Scale", this, 1.0, 0.0, 10.0);
+	public DoubleSetting scaleByDistance = new DoubleSetting("ScaleByDistance", this, 1,0,1);
+	ColorSetting color = new ColorSetting("Color", this, new Color(0xFFFFFFFF, true));
 
-    @SubscribeEvent
-    public void onRender3D(RenderWorldLastEvent event) {
-        if (this.isEnabled() && !nullCheck()) {
-        	for (Entity entity : Minecraft.getMinecraft().world.getLoadedEntityList()) {
-        		if (entity instanceof EntityTameable) {
-        			if (((EntityTameable) entity).isTamed()) {
-        				String ownerName = SessionUtils.getname(((EntityTameable) entity).getOwnerId());
-        				renderOwner(entity, ownerName);
-        			}
-        		} else if (entity instanceof AbstractHorse) {
-        			if (((AbstractHorse) entity).isTame()) {
-        				String ownerName = SessionUtils.getname(((AbstractHorse) entity).getOwnerUniqueId());
-        				renderOwner(entity, ownerName);
-        			}
-        		}
-        	}
-        }
-    }
-    
-    void renderOwner(Entity entity, String ownerName) {
-    	Vec3d center = entity.getPositionVector();
-    	GL11.glPushMatrix();
-    	RenderUtil.glBillboardDistanceScaled((float) center.x, (float) (center.y + 1.2), (float) center.z, mc.player, 1);
-    	String s = "Owned by: " + ownerName;
-    	GL11.glDisable(GL11.GL_DEPTH_TEST);
-    	mc.fontRenderer.drawStringWithShadow(s, -Spark.fontManager.getTextWidth(s)/2, 0, 0xffffff);
-    	GL11.glPopMatrix();
-    	GL11.glEnable(GL11.GL_DEPTH_TEST);
-    }
+	@SubscribeEvent
+	public void onRenderWorld(RenderWorldLastEvent event) {
+		if (nullCheck() || mc.renderEngine == null || mc.getRenderManager().options == null)
+			return;
+
+		for (Entity entity : Minecraft.getMinecraft().world.getLoadedEntityList()) {
+			if (entity instanceof EntityTameable) {
+				if (((EntityTameable) entity).isTamed()) {
+					String ownerName = SessionUtils.getname(((EntityTameable) entity).getOwnerId());
+					renderString(entity, "Owned by: " + ownerName, event.getPartialTicks());
+				}
+			} else if (entity instanceof AbstractHorse) {
+				if (((AbstractHorse) entity).isTame()) {
+					String ownerName = SessionUtils.getname(((AbstractHorse) entity).getOwnerUniqueId());
+					renderString(entity, "Owned by: " + ownerName, event.getPartialTicks());
+				}
+			}
+		}
+	}
+
+	void renderString(Entity entity, String displayString, float partialTicks) {
+		Entity viewEntity = mc.getRenderViewEntity();
+		Vec3d nametagPosition = RenderUtil.interpolateEntityByTicks(entity, partialTicks);
+
+		double x = nametagPosition.x;
+		double y = nametagPosition.y;
+		double z = nametagPosition.z;
+
+		nametagPosition = RenderUtil.interpolateEntityByTicks(viewEntity, partialTicks);
+
+		double posX = viewEntity.posX;
+		double posY = viewEntity.posY;
+		double posZ = viewEntity.posZ;
+
+		viewEntity.posX = nametagPosition.x;
+		viewEntity.posY = nametagPosition.y;
+		viewEntity.posZ = nametagPosition.z;
+
+		double distance = viewEntity.getDistance(x, y, z);
+
+		double distanceScale = scale.getValue() + (scale.getValue() / 5) * distance * scaleByDistance.getValue();
+
+		float width = mc.fontRenderer.getStringWidth(displayString) / 2;
+		float height = mc.fontRenderer.FONT_HEIGHT;
+
+		GlStateManager.pushMatrix();
+
+		GlStateManager.enablePolygonOffset();
+		GlStateManager.doPolygonOffset(1.0f, -1500000.0f);
+		GlStateManager.translate((float) x, (float) y + entity.height, (float) z);
+		GlStateManager.rotate(-mc.getRenderManager().playerViewY, 0.0f, 1.0f, 0.0f);
+		GlStateManager.rotate(mc.getRenderManager().playerViewX, (mc.gameSettings.thirdPersonView == 2) ? -1.0f : 1.0f, 0.0f, (float) 0);
+		GlStateManager.scale(-(distanceScale / 100), -(distanceScale / 100), (distanceScale / 100));
+
+		mc.fontRenderer.drawStringWithShadow(displayString, (int)-width + 1, (int)-height + 3, color.getRGB());
+
+		GlStateManager.pushMatrix();
+
+		GlStateManager.popMatrix();
+
+		GlStateManager.enableDepth();
+		GlStateManager.disableBlend();
+		GlStateManager.disablePolygonOffset();
+		GlStateManager.doPolygonOffset(1.0f, 1500000.0f);
+		GlStateManager.popMatrix();
+
+		mc.getRenderViewEntity().posX = posX;
+		mc.getRenderViewEntity().posY = posY;
+		mc.getRenderViewEntity().posZ = posZ;
+	}
 }
+
