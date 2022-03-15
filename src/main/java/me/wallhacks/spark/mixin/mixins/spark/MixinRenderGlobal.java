62730ec6a5953ffb50052c0bfb7cbb8b694554f5
jandvd <98705512+SparkClientSupport@users.noreply.github.com>
2022-03-15 23:55:03 +0100

    ukraine sky

--- src/main/java/me/wallhacks/spark/mixin/mixins/spark/MixinRenderGlobal.java
+++ src/main/java/me/wallhacks/spark/mixin/mixins/spark/MixinRenderGlobal.java
@@ -2,7 +2,10 @@ package me.wallhacks.spark.mixin.mixins.spark;
 
 import me.wallhacks.spark.Spark;
 import me.wallhacks.spark.event.render.RenderEntityEvent;
+import me.wallhacks.spark.event.render.SkyEvent;
+import me.wallhacks.spark.systems.module.modules.exploit.Zelensky;
 import me.wallhacks.spark.util.render.RenderUtil;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
@@ -22,6 +25,25 @@ public abstract class MixinRenderGlobal {
     @Inject(method = "renderEntities", at = @At("RETURN"))
     public void renderEntitiesReturn(CallbackInfo ci) {
         RenderUtil.isRenderLoop = false;
+    }
+
+
+    @Inject(method = "Lnet/minecraft/client/renderer/RenderGlobal;renderSky(FI)V", at = @At(value="HEAD"), cancellable = true)
+    public void renderSkyHead(float partialTicks, int pass,final CallbackInfo info) {
+
+        if(Minecraft.getMinecraft().player == null)
+            return;
+
+        SkyEvent skyEvent = new SkyEvent(partialTicks);
+
+
+        Spark.eventBus.post(skyEvent);
+        if(skyEvent.isCanceled())
+        {
+            info.cancel();
+        }
+
+
     }
 
 }
