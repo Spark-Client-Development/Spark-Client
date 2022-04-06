6289912197672bc4df016f8d8364c85e0102fdc7
TT392 <>
2022-04-06 21:53:42 +0200

    Clean up AutoPilot

--- src/main/java/me/wallhacks/spark/systems/module/modules/movement/AutoPilot.java
+++ src/main/java/me/wallhacks/spark/systems/module/modules/movement/AutoPilot.java
@@ -1,126 +1,81 @@
-package me.wallhacks.spark.systems.module.modules.player;
-
-import net.minecraft.network.play.client.CPacketPlayer.PositionRotation;
+package me.wallhacks.spark.systems.module.modules.movement;
 
 import java.util.ArrayList;
 
-import javax.vecmath.Vector2d;
-
-import org.lwjgl.BufferUtils;
-
-import com.sun.tools.javac.util.List;
-
-import akka.japi.Pair;
-import me.wallhacks.spark.event.player.PacketReceiveEvent;
-import me.wallhacks.spark.event.player.PacketSendEvent;
+import me.wallhacks.spark.event.player.PlayerUpdateEvent;
 import me.wallhacks.spark.event.player.PlayerUpdateMoveStateEvent;
 import me.wallhacks.spark.systems.module.Module;
 import me.wallhacks.spark.systems.setting.settings.BooleanSetting;
-import me.wallhacks.spark.systems.setting.settings.IntSetting;
 import me.wallhacks.spark.systems.setting.settings.DoubleSetting;
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.play.client.CPacketClickWindow;
-import net.minecraft.network.play.client.CPacketPlayer;
-import net.minecraft.network.play.server.SPacketPlayerPosLook;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 
-@Module.Registration(name = "AutoPilot", description = "Cancels server rotations")
+@Module.Registration(name = "AutoPilot", description = "Keeps elytraflying at the same altitude")
 public class AutoPilot extends Module {
-    DoubleSetting Ylevel = new DoubleSetting("Ylevel", this, 121.4, 0, 255);
-    DoubleSetting speedp = new DoubleSetting("speedp", this, 10, 0, 100);
-    DoubleSetting p = new DoubleSetting("p", this, .08, -1, 1);
-    DoubleSetting i = new DoubleSetting("i", this, .04, -1, 1);
-    DoubleSetting d = new DoubleSetting("d", this, 4, -1, 1);
-	BooleanSetting autostart = new BooleanSetting("AutoStart", this, false);
-    DoubleSetting jumpTime = new DoubleSetting("JumpTime", this, -.01, -1, 0);
-    IntSetting initialTime = new IntSetting("initialTime", this, 0, 0, 100);
-    DoubleSetting upSpeed = new DoubleSetting("upSpeed", this, 1, 0, 10);
+	BooleanSetting autoTakeoff = new BooleanSetting("AutoTakeoff", this, true);
+    DoubleSetting Ylevel = new DoubleSetting("Ylevel", this, 121.3, 0, 255);
+    double p = .08; // Proportional: determines how quickly the desired altitude is reached
+    double i = .04; // Integral: helps correct a final offset
+    double d = 4; // Differential: provides dampening to prevent overshoot
+    double speedP = 10; // Proportional control for pitch angle
 
-    ArrayList<Double> errorIntegralFifo = new ArrayList<Double>();
-
-
-    float pitch = 0;
-    double prevVspeed = 0;
-    
-    int tick = 0;
-    boolean gofly = false;
-    volatile double Vspeed;
-
-    @SubscribeEvent
-    public void tick(ClientTickEvent event) {
-        try {
-            Vspeed = Minecraft.getMinecraft().player.posY - Minecraft.getMinecraft().player.prevPosY;
-        } catch (Exception e) {
-        }
-    }
-
+    volatile double Vspeed = 0;
     @SubscribeEvent
     public void onWalkEvent(PlayerUpdateMoveStateEvent event) {
-        tick++;
-        if (tick % 20 == 0)
-            mc.player.movementInput.jump = true;
+        if (autoTakeoff.getValue()) {
+            if(mc.player.onGround)
+                mc.player.movementInput.jump = true;
 
-        System.out.println(String.valueOf(Vspeed));
-        System.out.println("test");
-        if (Vspeed < jumpTime.getValue())
-            mc.player.movementInput.jump = true;
-    }
-
-    int flyTicks = 0;
-    @SubscribeEvent
-    public void clientTick(ClientTickEvent event) {
-        try {
-
-            if (Minecraft.getMinecraft().player.isElytraFlying()) {
-                if (flyTicks < initialTime.getValue()) {
-                    if (mc.player.posY < Ylevel.getValue())
-                        mc.player.motionY = upSpeed.getValue();
-                }
-                flyTicks++;
-                double desiredVspeed = 0;
-                double maxPitch = 45;
-                double minPitch = -45;
-                double error = Minecraft.getMinecraft().player.posY - Ylevel.getValue();
-
-                int integralSize = 10;
-                errorIntegralFifo.add(error);
-                if (errorIntegralFifo.size() > integralSize) {
-                    errorIntegralFifo.remove(0);
-                }
-
-                double errorIntegral = 0;
-                for (int l = 0; l < errorIntegralFifo.size(); l++) {
-                    errorIntegral += errorIntegralFifo.get(l);
-                }
-                errorIntegral /= errorIntegralFifo.size();
-                
-
-                desiredVspeed = (Ylevel.getValue() - Minecraft.getMinecraft().player.posY)*(p.getValue()/10) + Vspeed*(d.getValue()/10) - errorIntegral*i.getValue();
-
-
-                pitch += (Vspeed - desiredVspeed) * speedp.getValue();
-                if (pitch > maxPitch) pitch = (float) maxPitch;
-                if (pitch < minPitch) pitch = (float) minPitch;
-
-
-
-                Minecraft.getMinecraft().player.rotationPitch = pitch;
-
-            }
-            else {
-                pitch = 0;
-                flyTicks = 0;
-                errorIntegralFifo.clear();
-            }
-        } catch (Exception e) {
+            if (Vspeed < -.01)
+                mc.player.movementInput.jump = true;
         }
-
     }
-    
+
+    ArrayList<Double> errorIntegralFifo = new ArrayList<Double>();
+    int flyTicks = 0;
+    float pitch = 0;
+    @SubscribeEvent
+    public void onUpdate(PlayerUpdateEvent event) {
+        Vspeed = Minecraft.getMinecraft().player.posY - Minecraft.getMinecraft().player.prevPosY;
+
+        if (Minecraft.getMinecraft().player.isElytraFlying()) {
+            if (flyTicks < 20) {
+                // Prevents hitting the ground on takeoff (helps with new anticheat)
+                if (mc.player.posY < Ylevel.getValue())
+                    mc.player.motionY = .1;
+            }
+            flyTicks++;
+
+            double maxPitch = 45;
+            double minPitch = -45;
+            double error = Minecraft.getMinecraft().player.posY - Ylevel.getValue();
+
+            int integralSize = 10;
+            errorIntegralFifo.add(error);
+            if (errorIntegralFifo.size() > integralSize) {
+                errorIntegralFifo.remove(0);
+            }
+
+            double errorIntegral = 0;
+            for (int l = 0; l < errorIntegralFifo.size(); l++) {
+                errorIntegral += errorIntegralFifo.get(l);
+            }
+            errorIntegral /= errorIntegralFifo.size();
+
+            double desiredVspeed = 0;
+            desiredVspeed = (Ylevel.getValue() - Minecraft.getMinecraft().player.posY)*(p/10) + Vspeed*(d/10) - errorIntegral*i;
+
+            pitch += (Vspeed - desiredVspeed) * speedP;
+            if (pitch > maxPitch) pitch = (float) maxPitch;
+            if (pitch < minPitch) pitch = (float) minPitch;
+
+            Minecraft.getMinecraft().player.rotationPitch = pitch;
+        }
+        else {
+            pitch = 0;
+            flyTicks = 0;
+            errorIntegralFifo.clear();
+        }
+    }
 }
